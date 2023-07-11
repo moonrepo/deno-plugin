538eb8dd09b950597393f6adc9be243cb7d047fc
Miles Johnson <mileswjohnson@gmail.com>
2023-07-10 19:30:22 -0700

    new: Update to latest PDK.

--- src/proto.rs
+++ src/proto.rs
@@ -1,6 +1,5 @@
 use extism_pdk::*;
 use proto_pdk::*;
-use serde::Deserialize;
 
 static NAME: &str = "Deno";
 static BIN: &str = "deno";
@@ -26,7 +25,7 @@ pub fn download_prebuilt(
         other => {
             return Err(PluginError::UnsupportedArchitecture {
                 tool: NAME.into(),
-                arch: format!("{:?}", other),
+                arch: other.to_string(),
             })?;
         }
     };
@@ -38,7 +37,7 @@ pub fn download_prebuilt(
         other => {
             return Err(PluginError::UnsupportedPlatform {
                 tool: NAME.into(),
-                platform: format!("{:?}", other),
+                platform: other.to_string(),
             })?;
         }
     };
@@ -62,43 +61,26 @@ pub fn locate_bins(Json(input): Json<LocateBinsInput>) -> FnResult<Json<LocateBi
         } else {
             BIN.into()
         }),
+        fallback_last_globals_dir: true,
         globals_lookup_dirs: vec![
-            "$DENO_INSTALL_ROOT/bin".into(),
+            "$DENO_INSTALL_ROOT".into(),
             "$DENO_HOME/bin".into(),
             "$HOME/.deno/bin".into(),
         ],
     }))
 }
 
-#[derive(Deserialize)]
-pub struct TagEntry {
-    name: String,
-}
-
 #[plugin_fn]
 pub fn load_versions(Json(_): Json<LoadVersionsInput>) -> FnResult<Json<LoadVersionsOutput>> {
-    let mut output = LoadVersionsOutput::default();
-    let mut latest = Version::new(0, 0, 0);
+    let tags = load_git_tags("https://github.com/denoland/deno")?;
 
-    let response: Vec<TagEntry> = fetch_url("https://api.github.com/repos/denoland/deno/tags")?;
-    let tags = response
+    let tags = tags
         .iter()
-        .filter_map(|entry| entry.name.strip_prefix('v'))
+        .filter(|t| !t.ends_with("^{}"))
+        .filter_map(|t| t.strip_prefix('v').map(|t| t.to_owned()))
         .collect::<Vec<_>>();
 
-    for tag in tags {
-        let version = Version::parse(tag)?;
-
-        if version > latest {
-            latest = version.clone();
-        }
-
-        output.versions.push(version);
-    }
-
-    output.aliases.insert("latest".into(), latest);
-
-    Ok(Json(output))
+    Ok(Json(LoadVersionsOutput::from_tags(&tags)?))
 }
 
 #[plugin_fn]
