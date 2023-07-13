ab89aec13a8f4543a917b78c50a42767ff7a299c
Miles Johnson <milesj@users.noreply.github.com>
2023-07-13 11:32:51 -0700

    tests: Add unit tests. (#1)
    * Scaffold.
    
    * Add tests.
    
    * Add more tests.
    
    * Fix panics.
    
    * Fix build.

---
+++ tests/download_test.rs
@@ -0,0 +1,204 @@
+use proto_pdk::*;
+use proto_pdk_test_utils::{create_plugin, generate_download_install_tests};
+use starbase_sandbox::create_empty_sandbox;
+use std::path::PathBuf;
+
+generate_download_install_tests!("deno-test", "1.30.0");
+
+#[test]
+#[should_panic(expected = "Unable to install Deno, unsupported architecture arm64 for linux.")]
+fn doesnt_support_linux_arm64() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin.download_prebuilt(DownloadPrebuiltInput {
+            env: Environment {
+                arch: HostArch::Arm64,
+                os: HostOS::Linux,
+                version: "1.2.0".into(),
+                ..Default::default()
+            }
+        }),
+        DownloadPrebuiltOutput {
+            archive_prefix: None,
+            bin_path: None,
+            checksum_name: None,
+            checksum_url: None,
+            download_name: Some("deno-aarch64-unknown-linux-gnu.zip".into()),
+            download_url:
+                "https://github.com/denoland/deno/releases/download/v1.2.0/deno-aarch64-unknown-linux-gnu.zip"
+                    .into()
+        }
+    );
+}
+
+#[test]
+fn supports_linux_x64() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin.download_prebuilt(DownloadPrebuiltInput {
+            env: Environment {
+                arch: HostArch::X64,
+                os: HostOS::Linux,
+                version: "1.2.0".into(),
+                ..Default::default()
+            }
+        }),
+        DownloadPrebuiltOutput {
+            archive_prefix: None,
+            bin_path: None,
+            checksum_name: None,
+            checksum_url: None,
+            download_name: Some("deno-x86_64-unknown-linux-gnu.zip".into()),
+            download_url: "https://github.com/denoland/deno/releases/download/v1.2.0/deno-x86_64-unknown-linux-gnu.zip".into()
+        }
+    );
+}
+
+#[test]
+fn supports_macos_arm64() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin.download_prebuilt(DownloadPrebuiltInput {
+            env: Environment {
+                arch: HostArch::Arm64,
+                os: HostOS::MacOS,
+                version: "1.2.0".into(),
+                ..Default::default()
+            }
+        }),
+        DownloadPrebuiltOutput {
+            archive_prefix: None,
+            bin_path: None,
+            checksum_name: None,
+            checksum_url: None,
+            download_name: Some("deno-aarch64-apple-darwin.zip".into()),
+            download_url:
+                "https://github.com/denoland/deno/releases/download/v1.2.0/deno-aarch64-apple-darwin.zip"
+                    .into()
+        }
+    );
+}
+
+#[test]
+fn supports_macos_x64() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin.download_prebuilt(DownloadPrebuiltInput {
+            env: Environment {
+                arch: HostArch::X64,
+                os: HostOS::MacOS,
+                version: "1.2.0".into(),
+                ..Default::default()
+            }
+        }),
+        DownloadPrebuiltOutput {
+            archive_prefix: None,
+            bin_path: None,
+            checksum_name: None,
+            checksum_url: None,
+            download_name: Some("deno-x86_64-apple-darwin.zip".into()),
+            download_url: "https://github.com/denoland/deno/releases/download/v1.2.0/deno-x86_64-apple-darwin.zip".into()
+        }
+    );
+}
+
+#[test]
+#[should_panic(expected = "Unable to install Deno, unsupported architecture arm64 for windows.")]
+fn doesnt_support_windows_arm64() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin.download_prebuilt(DownloadPrebuiltInput {
+            env: Environment {
+                arch: HostArch::Arm64,
+                os: HostOS::Windows,
+                version: "1.2.0".into(),
+                ..Default::default()
+            }
+        }),
+        DownloadPrebuiltOutput {
+            archive_prefix: None,
+            bin_path: None,
+            checksum_name: None,
+            checksum_url: None,
+            download_name: Some("deno-aarch64-pc-windows-msvc.zip".into()),
+            download_url: "https://github.com/denoland/deno/releases/download/v1.2.0/deno-aarch64-pc-windows-msvc.zip".into()
+        }
+    );
+}
+
+#[test]
+fn supports_windows_x64() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin.download_prebuilt(DownloadPrebuiltInput {
+            env: Environment {
+                arch: HostArch::X64,
+                os: HostOS::Windows,
+                version: "1.2.0".into(),
+                ..Default::default()
+            }
+        }),
+        DownloadPrebuiltOutput {
+            archive_prefix: None,
+            bin_path: None,
+            checksum_name: None,
+            checksum_url: None,
+            download_name: Some("deno-x86_64-pc-windows-msvc.zip".into()),
+            download_url: "https://github.com/denoland/deno/releases/download/v1.2.0/deno-x86_64-pc-windows-msvc.zip".into()
+        }
+    );
+}
+
+#[test]
+fn locates_unix_bin() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin
+            .locate_bins(LocateBinsInput {
+                env: Environment {
+                    arch: HostArch::Arm64,
+                    os: HostOS::Linux,
+                    version: "1.2.0".into(),
+                    ..Default::default()
+                },
+                tool_dir: PathBuf::new()
+            })
+            .bin_path,
+        Some("deno".into())
+    );
+}
+
+#[test]
+fn locates_windows_bin() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("bun-test", sandbox.path());
+
+    assert_eq!(
+        plugin
+            .locate_bins(LocateBinsInput {
+                env: Environment {
+                    arch: HostArch::X64,
+                    os: HostOS::Windows,
+                    version: "1.2.0".into(),
+                    ..Default::default()
+                },
+                tool_dir: PathBuf::new()
+            })
+            .bin_path,
+        Some("deno.exe".into())
+    );
+}
---
+++ tests/versions_test.rs
@@ -0,0 +1,31 @@
+use proto_pdk::*;
+use proto_pdk_test_utils::{create_plugin, generate_resolve_versions_tests};
+use starbase_sandbox::create_empty_sandbox;
+
+generate_resolve_versions_tests!("deno-test", {
+    "1.19" => "1.19.3",
+    "1.11" => "1.11.5",
+    "1.9.2" => "1.9.2",
+});
+
+#[test]
+fn loads_versions_from_git() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("deno-test", sandbox.path());
+
+    let output = plugin.load_versions(LoadVersionsInput::default());
+
+    assert!(!output.versions.is_empty());
+}
+
+#[test]
+fn sets_latest_alias() {
+    let sandbox = create_empty_sandbox();
+    let plugin = create_plugin("deno-test", sandbox.path());
+
+    let output = plugin.load_versions(LoadVersionsInput::default());
+
+    assert!(output.latest.is_some());
+    assert!(output.aliases.contains_key("latest"));
+    assert_eq!(output.aliases.get("latest"), output.latest.as_ref());
+}
