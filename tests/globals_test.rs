b246559fc8b68c87c7957f0df9c210e079fc2f64
Miles Johnson <milesj@users.noreply.github.com>
2023-08-15 16:06:21 -0700

    new: Support globals. (#3)

---
+++ tests/globals_test.rs
@@ -0,0 +1,6 @@
+// use proto_pdk_test_utils::*;
+// use starbase_sandbox::create_empty_sandbox;
+
+// This won't work because the macro requires a literal name for the dependency,
+// while Deno uses a URL.
+// generate_globals_test!("deno-test", "...");
